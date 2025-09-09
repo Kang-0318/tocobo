bc82291cc7463e8b1e5db1da77cee57684e7d952
Kang-0318 <audiogarden2@kbu.ac.kr>
2025-09-09 16:33:11 +0900

    r

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,71 @@
-// src/App.jsx
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Brand from "./sections/Brand";
-import Cta from "./sections/Cta";
-import Hero from "./sections/Hero";
-import Ingredients from "./sections/Ingredients";
-import Products from "./sections/Products";
-import Reviews from "./sections/Reviews";
-import "./styles/main.scss";
 
-export default function App() {
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Hero from "./sections/Hero"
+
+import Cta from "./sections/Cta"
+
+import "./styles/main.scss"
+import TopBanner from "./components/TopBanner"
+import { useState, useEffect } from "react"
+import TopBtn from "./components/TopBtn"
+import Hello from "./sections/Hello"
+import Collection from "./sections/Collection"
+import Skincare from "./sections/Skincare"
+import Instargram from "./sections/Instargram"
+function App() {
+
+  const [topBanner, setTopBanner] = useState("")
+
+  const [isScrolled, setIsScrolled] = useState(false)
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY
+
+      setIsScrolled(scrollTop > 0)
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+
+  })
+
+
+  const upTopBanner = () => {
+    setTopBanner("up")
+  }
+
   return (
-    <>
+    <div className={`app-container  ${topBanner} ${isScrolled ? "scrolled" : ""}`}>
+      <TopBtn />
+      <TopBanner onClick={upTopBanner} />
       <Header />
       <main>
-        <section id="hero"><Hero /></section>
-        <section id="brand"><Brand /></section>
-        <section id="products"><Products /></section>
-        <section id="ingredients"><Ingredients /></section>
-        <section id="reviews"><Reviews /></section>
-        <section id="contact"><Cta /></section>
+        <section id="Hero" className="Section">
+          <Hero />
+        </section>
+        <section id="Cta" className="Section">
+          <Cta />
+        </section>
+        <section id="Hello" className="Section">
+          <Hello />
+        </section>
+
+        <section id="Collection" className="Section">
+          <Collection />
+        </section>
+        <section id="Skincare" className="Section">
+          <Skincare />
+        </section>
+        <section id="Instar" className="Section">
+          <Instargram />
+        </section>
+
       </main>
       <Footer />
-    </>
-  );
+    </div>
+  )
 }
+
+export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Nav from './Nav'
+import Util from './Util'
 
+import { headerData } from '../util/header'
+import "../styles/components/header.scss"
 const Header = () => {
+
+  const headerLogo = headerData.logo
+
   return (
-    <div>Header</div>
+    <div>
+
+
+      <header>
+        <div className="inner">
+          <Nav />
+          <h1 className="tit">
+            <a href={headerLogo.href}>
+              <img src={headerLogo.src} alt={headerLogo.alt} />
+            </a>
+          </h1>
+          <Util />
+        </div>
+
+      </header>
+    </div>
   )
 }
 
